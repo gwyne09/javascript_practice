05696572149b9ebcc1f6c6fd2310a4852bc10cfc
gwyne09 <gwynejustin.deterala.jhs@ust.edu.ph>
2024-04-11 00:01:45 +0800

    ver 1.4
    added app loader (with bugs)

--- home/nav_bar.js
+++ home/nav_bar.js
@@ -8,6 +8,7 @@ const selected_app = document.querySelector('.nav_dropdown');
 
 // show nav_bar when hamburger button is clicked
 hamburger_button.onclick = (Element) => {
+    console.log("okay");
     if(nav_bar_elements.style.display === "none") {
         nav_bar_elements.style.display = "flex";
     } else {
@@ -41,11 +42,39 @@ const load_app = (html_loc, css_loc, js_loc) => {
         })
 };
 
-// load if clicked
-// load_timer.onclick = (event) => { // Correct the parameter name to 'event'
-//     event.preventDefault();
-//     load_app('/timer/timer.html');
-//     console.log("anchor clicked");
-// };
+// unload app function
+const unload_app = () => {
+    // Clear HTML content
+    display_app.innerHTML = '';
 
-load_app('/calculator/calculator.html', '/calculator/calculator.css', '/calculator/calculator.js')
+    // Remove dynamically loaded CSS file
+    const loadedCss = document.getElementById("loaded_app_css");
+    if (loadedCss) {
+        loadedCss.parentNode.removeChild(loadedCss);
+        console.log("css unloaded");
+    }
+
+    // Remove dynamically loaded JavaScript file
+    const loadedScript = document.getElementById("loaded_app_script");
+    if (loadedScript) {
+        loadedScript.parentNode.removeChild(loadedScript);
+        console.log("scripts unloaded");
+    }
+    // Disable all elements with class '.app'
+    document.querySelectorAll(".app").forEach(element => {
+        element.disabled = false;
+    });
+};
+
+
+// load if selected app is clicked
+// !! make sure the name of the app matches the locations in the repository
+selected_app.onclick = (event) => { 
+    if (display_app.innerHTML != '') {
+        unload_app();
+    }
+    const app = event.target.textContent.toLowerCase();
+    load_app(`/${app}/${app}.html`, `/${app}/${app}.css`, `/${app}/${app}.js`);
+    console.log(`${app} loaded`);
+    document.getElementById(app).disabled = true;
+};
